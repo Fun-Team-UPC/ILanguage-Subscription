2035554b421eec68625dc527d4d6a0476e7912aa
SebTou08 <sebaas14toulier@gmail.com>
2021-09-21 18:37:44 -0500

    feat: add subscription controller

--- src/main/java/com/edu/upc/ilanguagesubscription/command/api/SubscriptionCommandController.java
+++ src/main/java/com/edu/upc/ilanguagesubscription/command/api/SubscriptionCommandController.java
@@ -1,12 +1,70 @@
 package com.edu.upc.ilanguagesubscription.command.api;
 
+import com.edu.upc.ilanguagesubscription.command.application.dto.RegisterSubscriptionErrorResponse;
+import com.edu.upc.ilanguagesubscription.command.application.dto.RegisterSubscriptionOkResponse;
+import com.edu.upc.ilanguagesubscription.command.application.dto.RegisterSubscriptionRequestDto;
+import com.edu.upc.ilanguagesubscription.command.infra.SubscriptionInfra;
+import com.edu.upc.ilanguagesubscription.command.infra.SubscriptionRepository;
 import io.swagger.annotations.Api;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/subscriptions")
 @Api(tags = "Customers")
 public class SubscriptionCommandController {
+    private final CommandGateway _commandGateway;
+    private final SubscriptionRepository _subscriptionRepository;
 
+
+    public SubscriptionCommandController(CommandGateway commandGateway, SubscriptionRepository subscriptionRepository) {
+        _commandGateway = commandGateway;
+        _subscriptionRepository = subscriptionRepository;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Object> register(@RequestBody RegisterSubscriptionRequestDto registerSubscriptionRequestDto) {
+        Optional<SubscriptionInfra> existingSubscriptionInfra = _subscriptionRepository.findById(registerSubscriptionRequestDto.getSubscriptionId());
+        if (existingSubscriptionInfra.isPresent()) {
+            return new ResponseEntity(new RegisterSubscriptionErrorResponse(), HttpStatus.BAD_REQUEST);
+        }
+        int subscriptionId = Integer.parseInt(UUID.randomUUID().toString());
+
+        RegisterSubscription registerSubscription = new RegisterSubscription(
+                subscriptionId,
+                registerSubscriptionRequestDto.getName(),
+                registerSubscriptionRequestDto.getPrice(),
+                registerSubscriptionRequestDto.getMonthDuration()
+        );
+        CompletableFuture<Object> future = _commandGateway.send(registerSubscription);
+        CompletableFuture<Object> futureResponse = future.handle((ok, ex) -> {
+            if (ex != null) {
+                return new RegisterSubscriptionErrorResponse();
+            }
+            return new RegisterSubscriptionOkResponse(subscriptionId);
+        });
+
+        Object response = null;
+        try {
+            response = (Object) futureResponse.get();
+            if (response instanceof RegisterSubscriptionOkResponse) {
+                return new ResponseEntity(response, HttpStatus.CREATED);
+            }
+            return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        } catch (InterruptedException | ExecutionException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
+    }
 }
--- src/main/java/com/edu/upc/ilanguagesubscription/command/infra/SubscriptionRepository.java
+++ src/main/java/com/edu/upc/ilanguagesubscription/command/infra/SubscriptionRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface SubscriptionInterface extends JpaRepository<SubscriptionInfra, Integer> {
+public interface SubscriptionRepository extends JpaRepository<SubscriptionInfra, Integer> {
 
     //!Query refers to the class (MODEL)
     @Query("SELECT s FROM SubscriptionInfra s WHERE s.name = ?1")
